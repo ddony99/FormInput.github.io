fc3c63439ea81b3d2e4a3437f09dc0a653a18d26
Donatien Dony Luckner <104155242+ddony99@users.noreply.github.com>
2023-02-06 19:27:24 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -7,20 +7,42 @@ e.preventDefault()
 const email = document.querySelector('#email')
 const password = document.querySelector('#password')
 const showPassword = document.querySelector("#showpassword")
-const passwordConfirm = document.querySelector('#passwordConfirm')
+const passwordConfirm2 = document.querySelector('#passwordConfirm2')
 
 if (
     !email.checkValidity() ||
-    !password.checkValidity()
-) {
+    !password.checkValidity() ) {
     alert('Please fill out all fields correctly.')
     return
-}else{
+    }
+else{
     alert('Form submitted succesfully!')
 }
    
 })
+/* 
+ // check form valid request email/Password
 
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+e.preventDefault()
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const showPassword = document.querySelector("#showpassword")
+const passwordConfirm2 = document.querySelector('#passwordConfirm2')
+
+if (
+    !email.checkValidity() ||
+    !password.checkValidity() || passwordConfirm2.checkValidity()
+) {
+    alert('Please fill out all fields correctly.')
+    return
+}else{
+    prompt('Form submitted succesfully!')
+}
+   
+})
+ */
 
 /* const password = document.getElementById("password");
 const current = document.getElementById("current");
@@ -61,12 +83,12 @@ const passwordConfirm2 = document.querySelector('#passwordConfirm2')
 const showPassword2 = document.querySelector("#showpassword2")
 
 
-/*  //firstForm
+  //firstForm
 //password.value = "Doe"
 //toggle using click to make work cross browser
-showpassword.addEventListener("click", handleToggleClick, false);
+showpassword.addEventListener("click", handleToggleClick_Login, false);
 // toggle click, changing the Type of paswword input
-function handleToggleClick(event){
+function handleToggleClick_Login(event){
     if(this.checked){
         console.warn("Change input 'type' to : text")
         password.type = "text"
@@ -76,15 +98,14 @@ function handleToggleClick(event){
         password.type = "password"
       
     }
-    
-} */
+} 
 
 //SecondForm
 //password.value = "Doe"
 //toggle using click to make work cross browser
-showPassword2.addEventListener("click", handleToggleClick, false);
+showPassword2.addEventListener("click", handleToggleClick_signUp, false);
 // toggle click, changing the Type of paswword input
-function handleToggleClick(event){
+function handleToggleClick_signUp(event){
     if(this.checked){
         console.warn("Change input 'type' to : text")
         password2.type = "text"
@@ -95,3 +116,37 @@ function handleToggleClick(event){
         passwordConfirm2.type = "password"
     }
 }
+
+ //swipe FORM
+ var hidden = document.querySelector(".form_sign_up");
+ var hidden1 = document.querySelector(".form_login");
+ var login = document.querySelector("#login");
+ var SignUp = document.querySelector("#SignUp");
+ login.addEventListener("click", loginIn, false);
+ SignUp.addEventListener("click", SignUpIn, false);
+ // toggle click, changing the Type of paswword input
+ function loginIn(event){
+     if(this.click){
+         hidden1.removeAttribute("hidden");
+         login.setAttribute("hidden", "hidden");
+         SignUp.setAttribute("hidden", "hidden");
+     }
+     else{
+         
+         console.warn("Change input 'type' to passsord")
+         current.type = "link"
+     }
+ } 
+ function SignUpIn(event){
+   if(this.click){
+
+         hidden.removeAttribute("hidden");
+         login.setAttribute("hidden", "hidden");
+         SignUp.setAttribute("hidden", "hidden");
+     }
+     else{
+         
+         console.warn("Change input 'type' to passsord")
+         current.type = "link"
+     }
+ } 
